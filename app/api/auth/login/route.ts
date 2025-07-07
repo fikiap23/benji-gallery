8714b57863873d8899b3d212bf925dc3b8dff4e2
Fiki Aprian <fikiaprian23@gmail.com>
2025-07-07 12:18:04 +0700

    fix

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import bcrypt from 'bcrypt'
+import * as bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { db } from '@/lib/db'
 
--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import bcrypt from 'bcrypt'
+import * as bcrypt from 'bcrypt'
 import { db } from '@/lib/db' // Prisma instance
 
 export async function POST(req: NextRequest) {
